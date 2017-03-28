159a946f27180f3d4731f1bb1e834d280abbd494
lebedoleg <lebed.oleg.gma@gmail.com>
2017-03-28 17:19:56 +0300

    24.03.17 friday

---
+++ src/test/java/cucumberTestProject/DefinitionTestSuite_Blogs.java
@@ -0,0 +1,14 @@
+package cucumberTestProject;
+
+import cucumber.api.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(features=
+//        "src/test/resources/features/Ict4appsProductsPage.feature",
+//        "src/test/resources/features/Ict4appsSpecialOffersPage.feature",
+        "src/test/resources/features/Ict4appsBlogsPage.feature")
+//        "src/test/resources/features/Ict4appsContactUsPage.feature"
+
+public class DefinitionTestSuite_Blogs {}
