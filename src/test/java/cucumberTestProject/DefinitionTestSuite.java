8cbcf93507c4b5f9f91277abb0ee7cc46deb7454
lebedoleg <lebed.oleg.gma@gmail.com>
2017-04-04 16:24:11 +0300

    24.03.17 friday

--- src/test/java/cucumberTestProject/DefinitionTestSuite.java
+++ src/test/java/cucumberTestProject/DefinitionTestSuite.java
@@ -13,6 +13,7 @@ import org.junit.runner.RunWith;
         "src/test/resources/features/Ict4appsContactUsPage.feature",
         "src/test/resources/features/Ict4appsSignInPage.feature",
         "src/test/resources/features/Ict4appsCreateAccountPage.feature",
+        "src/test/resources/features/Ict4appsSearchPage.feature"
 //        "src/test/resources/features/Ict4appsCreateLocalizationPage.feature",
 //        "src/test/resources/features/Ict4appsCreateSearchPage.feature"
 
